a5bfb9719624a132a05acae34375685466db9b7c
Ana Cristina Torres Vargas <cristi_torres94@hotmail.com>
2018-09-06 16:41:25 -0500

    calculate serie of 10 numbers

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,4 +1,45 @@
 public class Fibonacci {
-    
 
+        public static class ICalculator{
+
+        private int[] fiboCalculator(int number){
+            if (number < 0){
+                System.out.println("Error: Fibonacci series does not calculate negative numbers");
+                System.exit(0);
+                return null;
+            }
+            int[] fibonacciSerie = new int[number];
+
+            fibonacciSerie[0]= 0;
+            fibonacciSerie[1]= 1;
+
+            if (number == 0)
+                return fibonacciSerie;
+            if (number == 1)
+                return fibonacciSerie;
+            
+
+            for(int i=2; i<number; i++){
+                fibonacciSerie[i] = fibonacciSerie[i -1] + fibonacciSerie[i-2];
+            }
+
+            return fibonacciSerie;
+        }
+
+        public void serie10(){
+            int number = 10;   
+            int[] array = fiboCalculator(number);
+            
+            for(int i = 0; i < number; i++){
+                System.out.println(array[i]);
+            }
+        }
+    }
+
+    public static void main(String[] args){
+        ICalculator calc = new ICalculator();
+        calc.serie10();
+
+    }
 }
+
